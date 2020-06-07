9d935b6e96f78e3d0440da55557a5865b8b71904
Norberto Mantohac <nmantohac82098@gmail.com>
2020-06-06 21:00:44 -0700

    npm installed font awesome and completed initial navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { Image, Navbar, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+// import LogoImg from '../../images/Navbar-Brand.jpg';
+// import LogoImg from '../../images/Navbar-Brand1.png';
+// import LogoImg from '../../images/Navbar-Brand2.png';
+// import LogoImg from '../../images/Navbar-Brand3.png';
+// import LogoImg from '../../images/Navbar-Brand4.png';
+// import LogoImg from '../../images/Navbar-Brand5.png';
+import LogoImg from '../../images/Navbar-Brand6.png';
+// import LogoImg1 from '../../images/Navbar-Logo.svg';
+// import './style.css';
 
-const Navbar = () => {
+const ReactNavbar = () => {
+  // Used to highlight the current page that the user is currently on in the navbar
+  const location = useLocation();
+
   return (
-    <h1>I am a Navbar!</h1>
+    <Navbar expand="lg" sticky="top" style={{ backgroundColor: '#ff8c19', height: '15vh' }}>
+
+      <Navbar.Brand as={Link} to="/">
+        <Image src={LogoImg} height="100" width="250" className="d-inline-block align-top logo" alt="Brand Logo" />
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      <Navbar.Collapse>
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/home" className={location.pathname === '/home' ? 'nav-link active' : 'nav-link'} style={{ color: 'black', fontSize: '24px' }}>
+            <FontAwesomeIcon icon={faHome} style={{ marginRight: '5px' }} />
+            Home
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default ReactNavbar;
