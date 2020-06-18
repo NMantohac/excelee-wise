e0ba986bb8f9a2deb7c671f79afeb448a0ea8d3f
Norberto Mantohac <nmantohac82098@gmail.com>
2020-06-18 13:41:26 -0700

    modified Home page styling by adding in jumbotron and removing carousel

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
+import { Jumbotron, Image, Container, Row, Col } from 'react-bootstrap';
+import LogoImg from '../../images/Navbar-Brand1.png';
 import bg1 from '../../images/bg-1.png';
 import bg2 from '../../images/bg-2.png';
 import bg3 from '../../images/bg-3.png';
@@ -11,7 +12,71 @@ import './style.css';
 const Home = () => {
   return (
     <div>
-      <Carousel fade interval="6000" touch>
+      <Jumbotron fluid className="jumbotron-background">
+        <Container>
+          <Image src={LogoImg} height="100" width="300" fluid className="d-inline-block align-top logo jumbotron-image" alt="Brand Logo" />
+          <p className="jumbotron-text">Assisting the Homeless</p>
+        </Container>
+      </Jumbotron>
+      <hr />
+      <Container style={{ fontFamily: 'Amatic SC, serif', textAlign: 'center' }}>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <Image src={bg1} height="300" width="500" alt="Raging Waterfall" />
+          </Col>
+          <Col>
+            <p style={{ fontSize: '64px', marginTop: '50px', backgroundColor: '#f5d4aa' }}>When troubles crossed</p>
+            <p style={{ fontSize: '64px', backgroundColor: '#f5d4aa' }}>path on you</p>
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <p style={{ fontSize: '64px', marginTop: '50px', backgroundColor: '#f5d4aa' }}>Hope is never</p>
+            <p style={{ fontSize: '64px', backgroundColor: '#f5d4aa' }}>forgotten</p>
+          </Col>
+          <Col>
+            <Image src={bg2} height="300" width="500" alt="Forest With Light" />
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <Image src={bg3} height="300" width="500" alt="Recovering Woman" />
+          </Col>
+          <Col>
+            <p style={{ fontSize: '64px', marginTop: '50px', backgroundColor: '#f5d4aa' }}>PHPLA the acronym</p>
+            <p style={{ fontSize: '64px', backgroundColor: '#f5d4aa' }}>of help</p>
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <p style={{ fontSize: '64px', marginTop: '50px', backgroundColor: '#f5d4aa' }}>Recovery</p>
+            <p style={{ fontSize: '64px', backgroundColor: '#f5d4aa' }}>is coming</p>
+          </Col>
+          <Col>
+            <Image src={bg4} height="300" width="500" alt="Heart Shape with Sun" />
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <Image src={bg5} height="300" width="500" alt="Woman with Sunset" />
+          </Col>
+          <Col>
+            <p style={{ fontSize: '64px', marginTop: '50px', backgroundColor: '#f5d4aa' }}>An afterhought</p>
+            <p style={{ fontSize: '64px', backgroundColor: '#f5d4aa' }}>of joy</p>
+          </Col>
+        </Row>
+        <Row style={{ marginBottom: '30px' }}>
+          <Col>
+            <p style={{ fontSize: '64px', marginTop: '50px', backgroundColor: '#f5d4aa' }}>Because</p>
+            <p style={{ fontSize: '64px', backgroundColor: '#f5d4aa' }}>we care</p>
+          </Col>
+          <Col>
+            <Image src={bg6} height="300" width="500" alt="Cute Child" />
+          </Col>
+        </Row>
+      </Container>
+
+      {/* <Carousel fade interval="6000" touch>
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -85,7 +150,7 @@ const Home = () => {
             <h3 style={{ fontSize: '64px' }}>Because We Care</h3>
           </Carousel.Caption>
         </Carousel.Item>
-      </Carousel>
+      </Carousel> */}
     </div>
   );
 };
