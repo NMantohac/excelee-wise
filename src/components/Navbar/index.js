9ec4402df524d5fd132ae8a7d8eb284c3a5908a3
Norberto Mantohac <nmantohac82098@gmail.com>
2020-06-18 15:43:55 -0700

    decreased navbar and navbar-logo size on scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { Image, Navbar, Nav, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneAlt, faUsers, faQuestionCircle, faDonate } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt, faUsers, faQuestionCircle, faHeart } from '@fortawesome/free-solid-svg-icons';
 // import LogoImg from '../../images/Navbar-Brand.jpg';
 import LogoImg from '../../images/Navbar-Brand1.png';
 // import LogoImg from '../../images/Navbar-Brand2.png';
@@ -13,15 +13,29 @@ import LogoImg from '../../images/Navbar-Brand1.png';
 // import LogoImg1 from '../../images/Navbar-Logo.svg';
 import './style.css';
 
+function scrollFunction() {
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+    document.querySelector('.navbar').style.padding = '2px 10px';
+    document.querySelector('.navbar-logo').style.height = '70px';
+    document.querySelector('.navbar-logo').style.width = '150px';
+  } else {
+    document.querySelector('.navbar').style.padding = '10px 10px';
+    document.querySelector('.navbar-logo').style.height = '100px';
+    document.querySelector('.navbar-logo').style.width = '220px';
+  }
+}
+
+window.onscroll = function () { scrollFunction(); };
+
 const ReactNavbar = () => {
   // Used to highlight the current page that the user is currently on in the navbar
   const location = useLocation();
 
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar bg="light" expand="lg" sticky="top" className="navbar">
 
       <Navbar.Brand as={Link} to="/" style={{ marginLeft: '5px' }}>
-        <Image src={LogoImg} height="50" width="200" fluid className="d-inline-block align-top logo" alt="Brand Logo" />
+        <Image src={LogoImg} fluid className="d-inline-block align-top logo navbar-logo" alt="Brand Logo" />
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -47,8 +61,8 @@ const ReactNavbar = () => {
               </Nav.Link>
             </li>
             <Nav.Link as={Link} to="/donate" className={location.pathname === '/donate' ? 'nav-link active' : 'nav-link'} style={{ fontSize: '18px' }}>
-              <Button variant="outline-danger" size="lg" style={{ marginTop: '-10px' }}>
-                <FontAwesomeIcon icon={faDonate} style={{ marginRight: '10px' }} />
+              <Button variant="outline-danger" size="lg" style={{ marginTop: '-10px', borderRadius: '25px' }}>
+                <FontAwesomeIcon icon={faHeart} style={{ marginRight: '10px' }} />
                 Donate
               </Button>
             </Nav.Link>
