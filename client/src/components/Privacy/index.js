7aa182ae0676db305eae68374c82c46a10f57bfd
Norberto Mantohac <nmantohac82098@gmail.com>
2020-06-29 15:25:24 -0700

    added jumbotron to privacy and terms & conditions pages

--- client/src/components/Privacy/index.js
+++ client/src/components/Privacy/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import './style.css';
 
 const Privacy = () => {
   return (
-    <h1>I am inside Privacy!</h1>
+    <div>
+      <Jumbotron className="privacy-jumbotron">
+          <Container>
+            <p className="privacy-jumbotron-text">Private Policy</p>
+          </Container>
+        </Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            <p style={{ textAlign: 'center' }}>This is the private policy page!</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
--- client/src/components/TermsAndConditions/index.js
+++ client/src/components/TermsAndConditions/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import './style.css';
 
 const TermsAndConditions = () => {
   return (
-    <h1>I am inside Terms and Conditions!</h1>
+    <div>
+      <Jumbotron className="terms-jumbotron">
+          <Container>
+            <p className="terms-jumbotron-text">Terms and Conditions</p>
+          </Container>
+        </Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            <p style={{ textAlign: 'center' }}>This is the terms and conditions page!</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
