671b9cd1639c7b319d2c02ac9f73c0b0b4404cc0
Norberto Mantohac <nmantohac82098@gmail.com>
2020-07-25 18:36:21 -0700

    added home message

---
+++ client/src/components/HomeMessage/HomeMessage.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+
+const HomeMessage = () => {
+  return (
+    <div>
+      <Container style={{ textAlign: 'center', backgroundColor: '#f2f2f2' }}>
+        <Row>
+          <Col style={{ fontSize: '16px', padding: '30px' }}>
+            <Fade big>
+              <p className="home-message-1">
+                By the name itself, our organization's purpose is to serve your needs.
+              </p>
+              <p className="home-message-2">
+                We consider your social situation, an honor of trust.
+              </p>
+              <p className="home-message-3">
+                We make every effort at PHPLA to find a solution for you by putting foremost your well-being in our hearts.
+              </p>
+              <p className="home-message-4">
+                With our kindhearted values, and in the spirit of collaborative helping, we change our quality of lives together.
+              </p>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default HomeMessage;
--- client/src/pages/Home/Home.component.jsx
+++ client/src/pages/Home/Home.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeJumbotron from '../../components/HomeJumbotron/HomeJumbotron.component';
 import HomeContent from '../../components/HomeContent/HomeContent.component';
+import HomeMessage from '../../components/HomeMessage/HomeMessage.component';
 import './Home.styles.css';
 
 const Home = () => {
   return (
     <div>
       <HomeJumbotron />
+      <HomeMessage />
       <hr />
       <HomeContent />
     </div>
